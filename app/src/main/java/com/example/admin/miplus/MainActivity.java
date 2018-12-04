da8cb98d59fd21ba147e8f78cfe548efafbfb095
Penguin <penjuinj@gmail.com>
2018-12-04 17:27:22 +0200

    Add button to open NavigationView

--- app/src/main/java/com/example/admin/miplus/MainActivity.java
+++ app/src/main/java/com/example/admin/miplus/MainActivity.java
@@ -1,28 +1,24 @@
 package com.example.admin.miplus;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Base64;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.admin.miplus.adapter.TabsPagerFragmentAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,16 +28,23 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private Button logout;
     private FirebaseAuth mAuth;
+    private TextView name;
+    private TextView email;
+    private ImageView logo;
+    private ConstraintLayout header;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
         setTheme(R.style.AppTheme);
         setContentView(LAYOUT);
+        initView();
         initTabs();
         initToolbar();
         initNavigationView();
-        logout = (Button) findViewById(R.id.log_out);
+        initHeader();
+
 
 
     }
@@ -64,18 +67,49 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         toolbar.inflateMenu(R.menu.menu);
+
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
     }
     private void initNavigationView(){
         Drawer_Layout = (DrawerLayout) findViewById(R.id.drawer_layout);
     }
+    private void initHeader(){
+        final FirebaseUser currentUser = mAuth.getCurrentUser();
+        String nametext = currentUser.getDisplayName();
+        String emailtext = currentUser.getEmail();
+       // name.setText(currentUser.getDisplayName());
+        //email.setText(emailtext, TextView.BufferType.EDITABLE);
+        //logo.setImageMatrix(currentUser.getPhotoUrl());
+    }
+    private void  initView(){
+        logout = (Button) findViewById(R.id.log_out);
+        name = (TextView) findViewById(R.id.user_name_google);
+        email = (TextView) findViewById(R.id.user_email_google);
+        logo = (ImageView) findViewById(R.id.user_logo_google);
+        header = (ConstraintLayout) findViewById(R.id.header);
+    }
 
 
     public void onClick(View view) {
-                final FirebaseUser currentUser = mAuth.getCurrentUser();
-                currentUser.delete();
+            final FirebaseUser currentUser = mAuth.getCurrentUser();
+            currentUser.delete();
                 Intent userIntent = new Intent(MainActivity.this, SplashActivity.class);
                 MainActivity.this.startActivity(userIntent);
                 FirebaseAuth.getInstance().signOut();
+                finish();
                 MainActivity.this.finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Drawer_Layout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
