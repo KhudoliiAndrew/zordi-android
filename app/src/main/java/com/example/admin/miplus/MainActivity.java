abdb8fe9c8020c407a90e4253289e96b66709b57
Олег <mr.olegyou@gmail.com>
2019-01-10 15:35:07 +0200

    Remake map, Spawn and Zoom on indicated location

--- app/src/main/java/com/example/admin/miplus/MainActivity.java
+++ app/src/main/java/com/example/admin/miplus/MainActivity.java
@@ -31,6 +31,7 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -68,12 +69,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         mAuth = FirebaseAuth.getInstance();
         setTheme(R.style.AppTheme);
-        setContentView(LAYOUT);
+        setContentView(R.layout.activity_main);
         initView();
         initTabs();
         initToolbar();
         initNavigationView();
         initHeader();
+        initMap();
+
 
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
@@ -92,9 +95,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         steps_info = (TextView) findViewById(R.id.steps_info);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
     }
 
     @Override
@@ -203,7 +204,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
         if(countSensor != null) {
             sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
-        } else {Toast.makeText(this, "Count sensor is not available!", Toast.LENGTH_LONG).show();
+        } else {Toast.makeText(this, "StepCount sensor is not available!", Toast.LENGTH_LONG).show();
             }
         }
 
@@ -225,19 +226,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+    private void initMap(){
+        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        goToLocation(-34, 151, 15);
     }
 
     private void goToLocation(double lat, double lng) {
         LatLng ll = new LatLng(lat, lng);
         CameraUpdate update = CameraUpdateFactory.newLatLng(ll);
+        mMap.moveCamera(update);
+    }
+
+    private void goToLocation(double lat, double lng, float zoom) {
+        LatLng ll = new LatLng(lat, lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, zoom);
         mMap.moveCamera(update);
     }
 }
