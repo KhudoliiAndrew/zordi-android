10b6a348c143a0986e6561b0d62fb1e77a49c2c2
Олег <mr.olegyou@gmail.com>
2019-03-26 17:53:11 +0200

    Test commit

--- app/src/main/java/com/example/admin/miplus/activity/activity_in_main/ThirdActivity.java
+++ app/src/main/java/com/example/admin/miplus/activity/activity_in_main/ThirdActivity.java
@@ -1,10 +1,18 @@
 package com.example.admin.miplus.activity.activity_in_main;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.admin.miplus.R;
 
@@ -13,6 +21,7 @@ public class ThirdActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        Log.d(">>>>>>", "StepCreate");
         super.onCreate(savedInstanceState);
         setContentView(LAYOUT);
     }
--- app/src/main/java/com/example/admin/miplus/fragment/ThirdFragment.java
+++ app/src/main/java/com/example/admin/miplus/fragment/ThirdFragment.java
@@ -1,18 +1,33 @@
 package com.example.admin.miplus.fragment;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.admin.miplus.R;
 
-public class ThirdFragment extends Fragment {
+public class ThirdFragment extends Fragment implements SensorEventListener {
     private static final int LAYOUT = R.layout.third_activity;
     private View view;
+
+    private TextView textView;
+
+    private SensorManager mSensorManager;
+
+    private Sensor mStepCounterSensor;
+
+    private Sensor mStepDetectorSensor;
 
     public static ThirdFragment getInstance(){
         Bundle args = new Bundle();
@@ -25,7 +40,58 @@ public class ThirdFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.d(">>>>>>", "StepCreate");
         view = inflater.inflate(LAYOUT, container, false);
+
+        textView = (TextView) view.findViewById(R.id.textview);
+
+        mSensorManager = (SensorManager)
+                getActivity().getSystemService(Context.SENSOR_SERVICE);
+        mStepCounterSensor = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        mStepDetectorSensor = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+
         return view;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor sensor = event.sensor;
+        float[] values = event.values;
+        int value = -1;
+
+        Log.d(">>>>>>", "StepDetected");
+
+        if (values.length > 0) {
+            value = (int) values[0];
+        }
+        if (sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            textView.setText("Step Counter Detected : " + value);
+        } else if (sensor.getType() == Sensor.TYPE_STEP_DETECTOR) {
+            // For test only. Only allowed value is 1.0 i.e. for step taken
+            textView.setText("Step Detector Detected : " + value);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public void onResume() {
+
+        super.onResume();
+        Log.d(">>>>>>", "StepResume");
+        mSensorManager.registerListener(this, mStepCounterSensor,  SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this, mStepDetectorSensor, SensorManager.SENSOR_DELAY_FASTEST);
+
+    }
+
+    public void onStop() {
+        super.onStop();
+        Log.d(">>>>>>", "StepStop");
+        mSensorManager.unregisterListener(this, mStepCounterSensor);
+        mSensorManager.unregisterListener(this, mStepDetectorSensor);
     }
 }
