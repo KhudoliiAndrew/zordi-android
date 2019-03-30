2d385af6b8c6677a2e64af350acc36926c4ce193
Pen <guin>
2019-03-31 00:28:59 +0300

    filled in steps information fragment, add charts library

--- app/src/main/java/com/example/admin/miplus/fragment/FirstWindow/StepsInformationFragment.java
+++ app/src/main/java/com/example/admin/miplus/fragment/FirstWindow/StepsInformationFragment.java
@@ -1,6 +1,7 @@
 package com.example.admin.miplus.fragment.FirstWindow;
 
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -18,6 +19,14 @@ import android.view.ViewGroup;
 
 import com.example.admin.miplus.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.view.LineChartView;
+
 public class StepsInformationFragment extends Fragment {
 
     @Nullable
@@ -25,6 +34,7 @@ public class StepsInformationFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.steps_information_activity, container, false);
         initToolbar();
+        initChart(view);
         return view;
     }
 
@@ -37,6 +47,28 @@ public class StepsInformationFragment extends Fragment {
         actionbar.setDisplayShowHomeEnabled(true);
     }
 
+    private void  initChart(View view){
+        List<PointValue> values = new ArrayList<PointValue>();
+        values.add(new PointValue(0.2f, 0.4f));
+        values.add(new PointValue(1, 2.5f));
+        values.add(new PointValue(2, 0));
+        values.add(new PointValue(3, 4));
+
+        //In most cased you can call data model methods in builder-pattern-like manner.
+        Line line = new Line(values).setColor(getResources().getColor(R.color.colorPrimary)).setCubic(true).setHasPoints(false);
+        List<Line> lines = new ArrayList<Line>();
+        lines.add(line);
+
+        LineChartData data = new LineChartData();
+        data.setLines(lines);
+
+        LineChartView chart = new LineChartView(getContext());
+        chart.setLineChartData(data);
+
+        LineChartView chartView = (LineChartView) view.findViewById(R.id.chart);
+        chartView.setLineChartData(data);
+    }
+
     @Override
     public void onDetach() {
         super.onDetach();
@@ -47,14 +79,4 @@ public class StepsInformationFragment extends Fragment {
         actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
         getActivity().getSupportFragmentManager().beginTransaction().remove(this).commit();
     }
-
-    private void closefragment() {
-        int count = getActivity().getSupportFragmentManager().getBackStackEntryCount();
-
-        if (count != 0) {
-            getActivity().getSupportFragmentManager().popBackStack();
-        }
-    }
-
-
 }
